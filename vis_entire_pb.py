276a6c151503f7c1b833da67d9327a4493a370b8
SupersonicBathroomRug <68907940+SupersonicBathroomRug@users.noreply.github.com>
2021-01-27 13:46:25 +0100

    set steps to 300

--- vis_entire_pb.py
+++ vis_entire_pb.py
@@ -89,7 +89,7 @@ def alt_vis_layers(gan_net, pb_path, work_dir, save_dir,active_goals,logit_goals
     os.makedirs(sub_folder, exist_ok=True)
     for i in range(1):
         # Define paths and skip if needed
-        src_file = os.path.join(work_dir, 'step0100.jpg') #change this if changing num steps in dream_projector
+        src_file = os.path.join(work_dir, 'step0300.jpg') #change this if changing num steps in dream_projector
         dst_path = os.path.join(sub_folder, 'op.jpg')
 
         # Run, save image, save video
